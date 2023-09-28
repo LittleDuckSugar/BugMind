d966cacdd7a504b4d364decf9b3d2cb7d21035e2
LittleDuckSugar <bernard_antoine@outlook.com>
2023-09-28 15:23:47 +0200

    Delete old useless work

--- API/bugmind_old/player.go
+++
@@ -1,8 +0,0 @@
-package bugmind
-
-import "BugMindAPI/models"
-
-// newPlayer generate a new player profil
-func newPlayer(name string) models.Player {
-	return models.Player{Name: name, HP: 3, BugMind: 2, Deck: make([]models.Card, 5), Hand: make([]models.Card, 5)}
-}
