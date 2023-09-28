8b6229217a0db21b7062d9f8b7d0d7ecf1cd2b76
LittleDuckSugar <bernard_antoine@outlook.com>
2023-09-28 11:58:53 +0200

    Password support simplified

--- API/models/room.go
+++ API/models/room.go
@@ -1,5 +1,8 @@
 package models
 
+import "golang.org/x/crypto/bcrypt"
+
+// Rooms contain all rooms
 var Rooms = make(map[string]Room)
 
 type Room struct {
@@ -16,4 +19,20 @@ type NewRoom struct {
 	Name      string `json:"name"`
 	Password  string `json:"password"`
 	MaxPlayer uint8  `json:"max-player"`
+}
+
+type EnterRoom struct {
+	Password   string `json:"password"`
+	PlayerName string `json:"player-name"`
+}
+
+// CheckPassword do what is means, check password
+func (room *Room) CheckPassword(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(room.Password), []byte(password))
+}
+
+// EncryptPassword do what is means, encrypt
+func (room *Room) EncryptPassword() {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(room.Password), 14)
+	room.Password = string(bytes)
 }
